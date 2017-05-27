9f261e1c2a2450e2a87e8c4194c6aaaf104b0f98
vicki alden <vic.alden23@gmail.com>
2017-05-27 16:20:10 -0700

    Add /api/chefs/meals route to index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -26,4 +26,41 @@ router.get('/api/users', function(req, res) {
   });
 });
 
+// chef's meals route
+router
+  .route('/api/chefs/meals')
+  .all(function(req, res, next) {
+    next();
+  })
+  .get(function(req, res, next) {
+    res.json({
+      results: [
+        {
+          id: 12345,
+          id_Chef: 12345,
+          name: 'Ramen',
+          description: 'Served with pork and veggies',
+          delivery_datetime: '2017-05-31 18:00:00',
+          pickup_info: 'Park in driveway',
+          price: 6,
+          servings: 10,
+          images: 'https://greatist.com/sites/default/files/SlowCooker-Pork-Ramen_0.jpg',
+          address: '123 main st',
+          city: 'Springfield',
+          state: 'CA',
+          zipcode: 99999,
+          rating: 5,
+          review_count: 10,
+          num_ordered: 2,
+          total_ordered: 30,
+          time_created: '2017-05-15 10:10:00',
+          time_updated: '2017-05-15 10:10:00'
+        }
+      ]
+    });
+    next();
+  })
+  .post(function(req, res) {
+    res.json(req.body);
+  });
 module.exports = router;
