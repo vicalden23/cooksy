c69b1651f3b6454ecf1925900679445fdaa6d2b8
vicki alden <vic.alden23@gmail.com>
2017-05-29 10:03:34 -0700

    add body-parser to server-config

--- src/server-config.js
+++ src/server-config.js
@@ -1,10 +1,13 @@
 var path = require('path');
 var express = require('express');
+var bodyParser = require('body-parser');
 var morgan = require('morgan');
 
 var indexRoutes = require('./routes/index');
 
 var app = express();
+
+app.use(bodyParser.json());
 
 if (process.env.NODE_ENV === 'production') {
   app.use(morgan('common'));
@@ -14,5 +17,7 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 app.use('/', indexRoutes);
+
+app.use('/api/chefs/meals');
 
 module.exports = app;
